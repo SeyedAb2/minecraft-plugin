c87fb9f7d8eefd8b95e921dc9db253b19d1cbae3
SeyedAb2 <tiggerwitht1378@gmail.com>
2022-06-17 10:36:31 +0430

    update code

--- src/main/java/seyed/ab2/finalproject/PlayerMovementListener.java
+++ src/main/java/seyed/ab2/finalproject/PlayerMovementListener.java
@@ -12,7 +12,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
+//import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerMovementListener implements Listener {
