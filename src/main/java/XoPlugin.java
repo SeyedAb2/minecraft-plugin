7381ef481e564c58ef90083b719db1e7bc34fb96
SeyedAb2 <tiggerwitht1378@gmail.com>
2022-07-08 14:29:47 -0300

    aaaa

---
+++ src/main/java/XoPlugin.java
@@ -0,0 +1,10 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XoPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Started!");
+        System.out.println("XO plugin minecraft");
+    }
+}
---
+++ src/main/java/minecraft/xoplugin/XoPlugin.java
@@ -0,0 +1,12 @@
+package minecraft.xoplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class XoPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Started!");
+        System.out.println("XO plugin minecraft");
+    }
+}
